4f265c3a6f2be9eec3c7339f9470249db229228c
Herve Peroteau <hpu@myfox.fr>
2014-04-17 18:45:03 +0200

    Initial commit from local git

---
+++ MFApi/MFApi.h
@@ -0,0 +1,13 @@
+//
+//  MFApi.h
+//  MFApi
+//
+//  Created by herve.peroteau on 17/04/2014.
+//  Copyright (c) 2014 MyFox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MFApi : NSObject
+
+@end
---
+++ MFApi/MFApi.m
@@ -0,0 +1,13 @@
+//
+//  MFApi.m
+//  MFApi
+//
+//  Created by herve.peroteau on 17/04/2014.
+//  Copyright (c) 2014 MyFox. All rights reserved.
+//
+
+#import "MFApi.h"
+
+@implementation MFApi
+
+@end
